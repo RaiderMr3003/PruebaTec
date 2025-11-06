5cd60bd47ef7957b84cfa4631ea81e7fa31f3f70
Maximo Mercado <mr3003gean@gmail.com>
2025-11-06 01:22:59 -0500

    Creación de los modulos de Trabajador. Script para manejar las peticiones ajax y css para mejorar el estilo que da bootstrap en móvil.

---
+++ wwwroot/js/Script_Trabajador.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $('#tablaTrabajadores').DataTable({
+        ajax: {
+            url: '/Trabajador/ListarTrabajador',
+            type: 'GET',
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'nombres' },
+            { data: 'apellidos' },
+            { data: 'tipoDocumento' },
+            { data: 'numeroDocumento' },
+            { data: 'sexo' },
+            { data: 'fechaNacimiento', render: d => new Date(d).toLocaleDateString() },
+            { data: 'direccion' },
+            {
+                data: 'idTrabajador',
+                render: function (id) {
+                    return `
+                    <div class="btn-group" role="group">
+                        <button class="btn btn-sm btn-warning btn-editar" data-id="${id}">
+                            <i class="bi bi-pencil-square"></i> Editar
+                        </button>
+                        <button class="btn btn-sm btn-danger btn-eliminar" data-id="${id}">
+                            <i class="bi bi-trash"></i> Eliminar
+                        </button>
+                    </div>
+                `;
+                },
+                orderable: false,
+                searchable: false
+            },
+        ]
+    });
+
+    $('#btnAddModalTrabajador').on('click', function () {
+        $('#addTrabajadorModal').modal('show');
+    });
+
+    $('#addTrabajadorModal').on('hidden.bs.modal', function () {
+        console.log('El modal se cerró');
+    });
+});

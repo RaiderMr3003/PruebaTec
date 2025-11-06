97447d7c68402ed7a810ba3909075f80070f8b7e
Maximo Mercado <mr3003gean@gmail.com>
2025-11-06 01:07:58 -0500

    Creación del metodo listarTrabajador para proximamente llamarlo mediante ajax desde js, para usar DataTable de jQuery

---
+++ Controllers/TrabajadorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaTec.Models;
+
+namespace PruebaTec.Controllers
+{
+    public class TrabajadorController : Controller
+    {
+        private readonly TrabajadoresPruebaContext _DBContext;
+
+        public TrabajadorController(TrabajadoresPruebaContext context)
+        {
+            _DBContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult listarTrabajador()
+        {
+            var trabajadores = _DBContext.Trabajadors.ToList();
+            return Ok(trabajadores);
+        }
+    }
+}
